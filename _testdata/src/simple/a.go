685bf8fb91a2dd785076266d904cd57fd326d761
Sam Boyer <tech@samboyer.org>
2016-06-20 11:30:42 -0400

    Add listPackage test and fixtures

---
+++ _testdata/src/simple/a.go
@@ -0,0 +1,12 @@
+package simple
+
+import (
+	"sort"
+
+	"github.com/sdboyer/vsolver"
+)
+
+var (
+	_ = sort.Strings
+	_ = vsolver.Solve
+)
--- analysis_test.go
+++ analysis_test.go
@@ -1,6 +1,8 @@
 package vsolver
 
 import (
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -113,4 +115,164 @@ func TestWorkmapToReach(t *testing.T) {
 			t.Errorf("wmToReach(%q): Did not get expected reach map:\n\t(GOT): %s\n\t(WNT): %s", name, out, fix.out)
 		}
 	}
+}
+
+func TestListPackages(t *testing.T) {
+	srcdir := filepath.Join(getwd(t), "_testdata", "src")
+	j := func(s string) string {
+		return filepath.Join(srcdir, s)
+	}
+
+	table := map[string]struct {
+		fileRoot   string // if left empty, will be filled to <cwd>/_testdata/src
+		importRoot string
+		tests      bool
+		out        []Package
+		err        error
+	}{
+		"code only": {
+			fileRoot:   j("simple"),
+			importRoot: "simple",
+			tests:      true,
+			out: []Package{
+				{
+					ImportPath:  "simple",
+					CommentPath: "",
+					Name:        "simple",
+					Imports: []string{
+						"github.com/sdboyer/vsolver",
+						"sort",
+					},
+				},
+			},
+		},
+		"test only": {
+			fileRoot:   j("t"),
+			importRoot: "simple",
+			tests:      true,
+			out: []Package{
+				{
+					ImportPath:  "simple",
+					CommentPath: "",
+					Name:        "simple",
+					TestImports: []string{
+						"math/rand",
+						"strconv",
+					},
+				},
+			},
+		},
+		"xtest only": {
+			fileRoot:   j("xt"),
+			importRoot: "simple",
+			tests:      true,
+			out: []Package{
+				{
+					ImportPath:  "simple",
+					CommentPath: "",
+					Name:        "simple",
+					TestImports: []string{
+						"sort",
+						"strconv",
+					},
+				},
+			},
+		},
+		"code and test": {
+			fileRoot:   j("simplet"),
+			importRoot: "simple",
+			tests:      true,
+			out: []Package{
+				{
+					ImportPath:  "simple",
+					CommentPath: "",
+					Name:        "simple",
+					Imports: []string{
+						"github.com/sdboyer/vsolver",
+						"sort",
+					},
+					TestImports: []string{
+						"math/rand",
+						"strconv",
+					},
+				},
+			},
+		},
+		"code and xtest": {
+			fileRoot:   j("simplext"),
+			importRoot: "simple",
+			tests:      true,
+			out: []Package{
+				{
+					ImportPath:  "simple",
+					CommentPath: "",
+					Name:        "simple",
+					Imports: []string{
+						"github.com/sdboyer/vsolver",
+						"sort",
+					},
+					TestImports: []string{
+						"sort",
+						"strconv",
+					},
+				},
+			},
+		},
+		"code, test, xtest": {
+			fileRoot:   j("simpleallt"),
+			importRoot: "simple",
+			tests:      true,
+			out: []Package{
+				{
+					ImportPath:  "simple",
+					CommentPath: "",
+					Name:        "simple",
+					Imports: []string{
+						"github.com/sdboyer/vsolver",
+						"sort",
+					},
+					TestImports: []string{
+						"math/rand",
+						"sort",
+						"strconv",
+					},
+				},
+			},
+		},
+	}
+
+	for name, fix := range table {
+		if _, err := os.Stat(fix.fileRoot); err != nil {
+			t.Errorf("listPackages(%q): error on fileRoot %s: %s", name, fix.fileRoot, err)
+			continue
+		}
+
+		out, err := listPackages(fix.fileRoot, fix.importRoot)
+
+		if fix.out == nil {
+			if err == nil {
+				t.Errorf("listPackages(%q): Error expected but not received", name)
+			} else if !reflect.DeepEqual(fix.err, err) {
+				t.Errorf("listPackages(%q): Did not receive expected error:\n\t(GOT): %s\n\t(WNT): %s", name, err, fix.err)
+			}
+			continue
+		}
+
+		if err != nil {
+			t.Errorf("listPackages(%q): %v", name, err)
+			continue
+		}
+
+		if !reflect.DeepEqual(out, fix.out) {
+			t.Errorf("listPackages(%q): Did not get expected package set:\n\t(GOT): %s\n\t(WNT): %s", name, out, fix.out)
+		}
+	}
+}
+
+func getwd(t *testing.T) string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return cwd
 }
