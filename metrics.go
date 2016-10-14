668502d3410d13962d39a3389a07d792f7e77189
sam boyer <tech@samboyer.org>
2016-10-13 23:33:33 -0400

    Instrument metrics across solver

--- metrics.go
+++ metrics.go
@@ -20,15 +20,15 @@ func newMetrics() *metrics {
 
 func (m *metrics) push(name string) {
 	cn := m.stack[len(m.stack)-1]
-	times[cn] = times[cn] + time.Since(m.last)
+	m.times[cn] = m.times[cn] + time.Since(m.last)
 
 	m.stack = append(m.stack, name)
 	m.last = time.Now()
 }
 
 func (m *metrics) pop() {
-	on = m.stack[len(m.stack)-1]
-	times[on] = times[on] + time.Since(m.last)
+	on := m.stack[len(m.stack)-1]
+	m.times[on] = m.times[on] + time.Since(m.last)
 
 	m.stack = m.stack[:len(m.stack)-1]
 	m.last = time.Now()
