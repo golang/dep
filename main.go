c6e8d4b16b413bf3332c6be501e5b37cd7967ab5
Edward Muller <zbintliff@gmail.com>
2016-11-29 16:41:16 -0800

    Move init to its own file

--- main.go
+++ main.go
@@ -102,23 +102,6 @@ func noop(args []string) error {
 // The following command declarations should be moved to their own files as
 // they are implemented.
 
-var initCmd = &command{
-	fn:   noop,
-	name: "init",
-	short: `
-	Write Manifest file in the root of the project directory.
-	`,
-	long: `
-	Populates Manifest file with current deps of this project.
-	The specified version of each dependent repository is the version
-	available in the user's workspaces (as specified by GOPATH).
-	If the dependency is not present in any workspaces it is not be
-	included in the Manifest.
-	Writes Lock file(?)
-	Creates vendor/ directory(?)
-	`,
-}
-
 var getCmd = &command{
 	fn:   noop,
 	name: "get",
