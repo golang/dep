262d4c3b526606d30ee2d1aec46b4aaf01c00418
Edward Muller <zbintliff@gmail.com>
2016-11-29 17:03:08 -0800

    Write a blank manifest

--- init.go
+++ init.go
@@ -1,7 +1,14 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 var initCmd = &command{
-	fn:   noop,
+	fn:   runInit,
 	name: "init",
 	short: `
 	Write Manifest file in the root of the project directory.
@@ -15,4 +22,43 @@ var initCmd = &command{
 	Writes Lock file(?)
 	Creates vendor/ directory(?)
 	`,
+}
+
+func runInit(args []string) error {
+	if len(args) > 1 {
+		return fmt.Errorf("Too many args: %d", len(args))
+	}
+	var p string
+	var err error
+	if len(args) == 0 {
+		p, err = os.Getwd()
+		if err != nil {
+			return err
+		}
+	} else {
+		p = args[0]
+	}
+
+	m := filepath.Join(p, manifestName)
+
+	// TODO: Lstat ? Do we care?
+	_, err = os.Stat(m)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return createManifest(m)
+		}
+		return err
+	}
+
+	return fmt.Errorf("Manifest file '%s' already exists", m)
+}
+
+func createManifest(path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	e := json.NewEncoder(f)
+	return e.Encode(newRawManifest())
 }
