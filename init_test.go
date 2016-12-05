0db465e279d237de497d6950cac26cd2da9175c2
Jess Frazelle <acidburn@google.com>
2016-12-05 13:17:02 -0800

    add initial init test
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- init_test.go
+++ init_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os/exec"
+	"testing"
+)
 
 func TestContains(t *testing.T) {
 	a := []string{"a", "b", "abcd"}
@@ -27,5 +30,100 @@ func TestIsStdLib(t *testing.T) {
 		if b != e {
 			t.Fatalf("%s: expected %t got %t", p, e, b)
 		}
+	}
+}
+
+func TestInit(t *testing.T) {
+	// test must also have external network
+	if _, err := exec.LookPath("git"); err != nil {
+		t.Skip("skipping because git binary not found")
+	}
+
+	tg := testgo(t)
+	defer tg.cleanup()
+
+	tg.tempDir("src")
+	tg.setenv("GOPATH", tg.path("."))
+
+	importPaths := map[string]string{
+		"github.com/pkg/errors":      "v0.8.0",                                   // semver
+		"github.com/Sirupsen/logrus": "42b84f9ec624953ecbf81a94feccb3f5935c5edf", // random sha
+	}
+
+	// checkout the specified revisions
+	for ip, rev := range importPaths {
+		tg.runGo("get", ip)
+		repoDir := tg.path("src/" + ip)
+		tg.runGit(repoDir, "checkout", rev)
+	}
+
+	m := `package main
+
+import (
+	"github.com/Sirupsen/logrus"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	err := nil
+	if err != nil {
+		errors.Wrap(err, "thing")
+	}
+	logrus.Info("hello world")
+}`
+
+	tg.tempFile("src/thing/thing.go", m)
+	tg.cd(tg.path("src/thing"))
+
+	tg.run("init")
+
+	expectedManifest := `{
+    "dependencies": {
+        "github.com/Sirupsen/logrus": {
+            "revision": "42b84f9ec624953ecbf81a94feccb3f5935c5edf"
+        },
+        "github.com/pkg/errors": {
+            "version": ">=0.8.0, <1.0.0"
+        }
+    }
+}
+`
+	manifest := tg.readManifest()
+	if manifest != expectedManifest {
+		t.Fatalf("expected %s, got %s", expectedManifest, manifest)
+	}
+
+	expectedLock := `{
+    "memo": "0d682aa197bf6e94b9af8298acea5b7957b9a5674570cc16b3e8436846928a57",
+    "projects": [
+        {
+            "name": "github.com/Sirupsen/logrus",
+            "revision": "42b84f9ec624953ecbf81a94feccb3f5935c5edf",
+            "packages": [
+                "."
+            ]
+        },
+        {
+            "name": "github.com/pkg/errors",
+            "version": "v0.8.0",
+            "revision": "645ef00459ed84a119197bfb8d8205042c6df63d",
+            "packages": [
+                "."
+            ]
+        },
+        {
+            "name": "golang.org/x/sys",
+            "branch": "master",
+            "revision": "478fcf54317e52ab69f40bb4c7a1520288d7f7ea",
+            "packages": [
+                "unix"
+            ]
+        }
+    ]
+}
+`
+	lock := tg.readLock()
+	if lock != expectedLock {
+		t.Fatalf("expected %s, got %s", expectedLock, lock)
 	}
 }
