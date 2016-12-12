57ee55a19368b8cb7e8075c504ae13d8799c8775
Jess Frazelle <acidburn@google.com>
2016-12-12 15:12:45 -0800

    add more tests for context.
    Signed-off-by: Jess Frazelle <acidburn@google.com>

--- init_test.go
+++ init_test.go
@@ -5,55 +5,9 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"path/filepath"
 	"regexp"
 	"testing"
-
-	"github.com/sdboyer/gps"
 )
-
-func TestAbsoluteProjectRoot(t *testing.T) {
-	tg := testgo(t)
-	defer tg.cleanup()
-
-	tg.tempDir("src")
-	tg.setenv("GOPATH", tg.path("."))
-	depCtx := &ctx{GOPATH: tg.path(".")}
-
-	importPaths := map[string]bool{
-		"github.com/pkg/errors": true,
-		"my/silly/thing":        false,
-	}
-
-	for i, create := range importPaths {
-		if create {
-			tg.tempDir(filepath.Join("src", i))
-		}
-	}
-
-	for i, ok := range importPaths {
-		pr, err := depCtx.absoluteProjectRoot(i)
-		if ok {
-			tg.must(err)
-			expected := tg.path(filepath.Join("src", i))
-			if pr != expected {
-				t.Fatalf("expected %s, got %q", expected, pr)
-			}
-			continue
-		}
-
-		if err == nil {
-			t.Fatalf("expected %s to fail", i)
-		}
-	}
-
-	// test that a file fails
-	tg.tempFile("src/thing/thing.go", "hello world")
-	_, err := depCtx.absoluteProjectRoot("thing/thing.go")
-	if err == nil {
-		t.Fatal("error should not be nil for a file found")
-	}
-}
 
 func TestContains(t *testing.T) {
 	a := []string{"a", "b", "abcd"}
@@ -79,37 +33,6 @@ func TestIsStdLib(t *testing.T) {
 		b := isStdLib(p)
 		if b != e {
 			t.Fatalf("%s: expected %t got %t", p, e, b)
-		}
-	}
-}
-
-func TestVersionInWorkspace(t *testing.T) {
-	needsExternalNetwork(t)
-	needsGit(t)
-
-	tg := testgo(t)
-	defer tg.cleanup()
-
-	tg.tempDir("src")
-	tg.setenv("GOPATH", tg.path("."))
-	depCtx := &ctx{GOPATH: tg.path(".")}
-
-	importPaths := map[string]gps.Version{
-		"github.com/pkg/errors":      gps.NewVersion("v0.8.0").Is("645ef00459ed84a119197bfb8d8205042c6df63d"), // semver
-		"github.com/Sirupsen/logrus": gps.Revision("42b84f9ec624953ecbf81a94feccb3f5935c5edf"),                // random sha
-	}
-
-	// checkout the specified revisions
-	for ip, rev := range importPaths {
-		tg.runGo("get", ip)
-		repoDir := tg.path("src/" + ip)
-		tg.runGit(repoDir, "checkout", rev.String())
-
-		v, err := depCtx.versionInWorkspace(gps.ProjectRoot(ip))
-		tg.must(err)
-
-		if v != rev {
-			t.Fatalf("expected %s, got %s", v.String(), rev.String())
 		}
 	}
 }
