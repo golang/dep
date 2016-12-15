668326785b8ae1bd0ccab0ef2e41298ddd824e07
sam boyer <tech@samboyer.org>
2016-12-14 19:56:27 -0500

    Replace stdlib literal list w/pattern checker
    This mirrors what's done in stdlib. Might not be the best long-term
    solution, but it's OK for now.

--- analysis.go
+++ analysis.go
@@ -16,34 +16,28 @@ import (
 
 var osList []string
 var archList []string
-var stdlib = make(map[string]bool)
-
-const stdlibPkgs string = "archive archive/tar archive/zip bufio builtin bytes compress compress/bzip2 compress/flate compress/gzip compress/lzw compress/zlib container container/heap container/list container/ring context crypto crypto/aes crypto/cipher crypto/des crypto/dsa crypto/ecdsa crypto/elliptic crypto/hmac crypto/md5 crypto/rand crypto/rc4 crypto/rsa crypto/sha1 crypto/sha256 crypto/sha512 crypto/subtle crypto/tls crypto/x509 crypto/x509/pkix database database/sql database/sql/driver debug debug/dwarf debug/elf debug/gosym debug/macho debug/pe debug/plan9obj encoding encoding/ascii85 encoding/asn1 encoding/base32 encoding/base64 encoding/binary encoding/csv encoding/gob encoding/hex encoding/json encoding/pem encoding/xml errors expvar flag fmt go go/ast go/build go/constant go/doc go/format go/importer go/parser go/printer go/scanner go/token go/types hash hash/adler32 hash/crc32 hash/crc64 hash/fnv html html/template image image/color image/color/palette image/draw image/gif image/jpeg image/png index index/suffixarray io io/ioutil log log/syslog math math/big math/cmplx math/rand mime mime/multipart mime/quotedprintable net net/http net/http/cgi net/http/cookiejar net/http/fcgi net/http/httptest net/http/httputil net/http/pprof net/mail net/rpc net/rpc/jsonrpc net/smtp net/textproto net/url os os/exec os/signal os/user path path/filepath reflect regexp regexp/syntax runtime runtime/cgo runtime/debug runtime/msan runtime/pprof runtime/race runtime/trace sort strconv strings sync sync/atomic syscall testing testing/iotest testing/quick text text/scanner text/tabwriter text/template text/template/parse time unicode unicode/utf16 unicode/utf8 unsafe"
-
-// Before appengine moved to google.golang.org/appengine, it had a magic
-// stdlib-like import path. We have to ignore all of these.
-const appenginePkgs string = "appengine/aetest appengine/blobstore appengine/capability appengine/channel appengine/cloudsql appengine/cmd appengine/cmd/aebundler appengine/cmd/aedeploy appengine/cmd/aefix appengine/datastore appengine/delay appengine/demos appengine/demos/guestbook appengine/demos/guestbook/templates appengine/demos/helloworld appengine/file appengine/image appengine/internal appengine/internal/aetesting appengine/internal/app_identity appengine/internal/base appengine/internal/blobstore appengine/internal/capability appengine/internal/channel appengine/internal/datastore appengine/internal/image appengine/internal/log appengine/internal/mail appengine/internal/memcache appengine/internal/modules appengine/internal/remote_api appengine/internal/search appengine/internal/socket appengine/internal/system appengine/internal/taskqueue appengine/internal/urlfetch appengine/internal/user appengine/internal/xmpp appengine/log appengine/mail appengine/memcache appengine/module appengine/remote_api appengine/runtime appengine/search appengine/socket appengine/taskqueue appengine/urlfetch appengine/user appengine/xmpp"
 
 func init() {
 	// The supported systems are listed in
 	// https://github.com/golang/go/blob/master/src/go/build/syslist.go
-	// The lists are not exported so we need to duplicate them here.
+	// The lists are not exported, so we need to duplicate them here.
 	osListString := "android darwin dragonfly freebsd linux nacl netbsd openbsd plan9 solaris windows"
 	osList = strings.Split(osListString, " ")
 
 	archListString := "386 amd64 amd64p32 arm armbe arm64 arm64be ppc64 ppc64le mips mipsle mips64 mips64le mips64p32 mips64p32le ppc s390 s390x sparc sparc64"
 	archList = strings.Split(archListString, " ")
+}
 
-	for _, pkg := range strings.Split(stdlibPkgs, " ") {
-		stdlib[pkg] = true
-	}
-	for _, pkg := range strings.Split(appenginePkgs, " ") {
-		stdlib[pkg] = true
+// This was loving taken from src/cmd/go/pkg.go in Go's code (isStandardImportPath).
+//
+// Stored as a var so that tests can swap it out. Ugh globals, ugh.
+var isStdLib = func(path string) bool {
+	i := strings.Index(path, "/")
+	if i < 0 {
+		i = len(path)
 	}
 
-	// Also ignore C
-	// TODO(sdboyer) actually figure out how to deal with cgo
-	stdlib["C"] = true
+	return !strings.Contains(path[:i], ".")
 }
 
 // ListPackages reports Go package information about all directories in the tree
