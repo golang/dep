c1e17554b37bd83e634598bb1a299c8f87eed929
Jaana Burcu Dogan <zbintliff@gmail.com>
2017-01-25 15:34:18 -0800

    rename hoard to deb

--- analyzer.go
+++ analyzer.go
@@ -14,12 +14,12 @@ import (
 
 type analyzer struct{}
 
-// TODO: If we decide to support other tools manifest, this is where we would need
-// to add that support.
 func (a analyzer) DeriveManifestAndLock(path string, n gps.ProjectRoot) (gps.Manifest, gps.Lock, error) {
+	// TODO: If we decide to support other tools manifest, this is where we would need
+	// to add that support.
 	mf := filepath.Join(path, manifestName)
 	if fileOK, err := isRegular(mf); err != nil || !fileOK {
-		// Do not return an error, when does not exist
+		// Do not return an error, when does not exist.
 		return nil, nil, nil
 	}
 	f, err := os.Open(mf)
@@ -32,12 +32,12 @@ func (a analyzer) DeriveManifestAndLock(path string, n gps.ProjectRoot) (gps.Man
 	if err != nil {
 		return nil, nil, err
 	}
-	// TODO: no need to return lock til we decide about preferred versions
-	// https://github.com/sdboyer/gps/wiki/gps-for-Implementors#preferred-versions
+	// TODO: No need to return lock til we decide about preferred versions, see
+	// https://github.com/sdboyer/gps/wiki/gps-for-Implementors#preferred-versions.
 	return m, nil, nil
 }
 
 func (a analyzer) Info() (name string, version *semver.Version) {
 	v, _ := semver.NewVersion("v0.0.1")
-	return "hoard", v
+	return "deb", v
 }
