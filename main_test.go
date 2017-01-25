f81d6f5e2612d99738eb5f09c1cb5ab1ddeaefb5
Jaana Burcu Dogan <jbd@google.com>
2017-01-24 23:29:04 -0800

    fix obvious readability problems

--- main_test.go
+++ main_test.go
@@ -5,7 +5,6 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -19,48 +18,41 @@ func TestFindRoot(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	expect := filepath.Join(wd, "_testdata", "rootfind")
-
-	got1, err := findProjectRoot(expect)
+	want := filepath.Join(wd, "_testdata", "rootfind")
+	got1, err := findProjectRoot(want)
 	if err != nil {
 		t.Errorf("Unexpected error while finding root: %s", err)
-	} else if expect != got1 {
-		t.Errorf("findProjectRoot directly on root dir should have found %s, got %s", expect, got1)
+	} else if want != got1 {
+		t.Errorf("findProjectRoot directly on root dir should have found %s, got %s", want, got1)
 	}
 
-	got2, err := findProjectRoot(filepath.Join(expect, "subdir"))
+	got2, err := findProjectRoot(filepath.Join(want, "subdir"))
 	if err != nil {
 		t.Errorf("Unexpected error while finding root: %s", err)
-	} else if expect != got2 {
-		t.Errorf("findProjectRoot on subdir should have found %s, got %s", expect, got2)
+	} else if want != got2 {
+		t.Errorf("findProjectRoot on subdir should have found %s, got %s", want, got2)
 	}
 
-	got3, err := findProjectRoot(filepath.Join(expect, "nonexistent"))
+	got3, err := findProjectRoot(filepath.Join(want, "nonexistent"))
 	if err != nil {
 		t.Errorf("Unexpected error while finding root: %s", err)
-	} else if expect != got3 {
-		t.Errorf("findProjectRoot on nonexistent subdir should still work and give %s, got %s", expect, got3)
+	} else if want != got3 {
+		t.Errorf("findProjectRoot on nonexistent subdir should still work and give %s, got %s", want, got3)
 	}
 
 	root := "/"
-	returnedPath, projectRootErr := findProjectRoot(root)
-	if returnedPath != "" {
-		t.Errorf("findProjectRoot with path %s returned non empty string: %s", root, returnedPath)
+	p, err := findProjectRoot(root)
+	if p != "" {
+		t.Errorf("findProjectRoot with path %s returned non empty string: %s", root, p)
 	}
-	if projectRootErr == nil {
-		t.Errorf("findProjectRoot with path %s should return error", root)
-	}
-	errStr := fmt.Sprintf("%v", projectRootErr.Error())
-	expectedStr := "could not find project manifest.json, use dep init to initiate a manifest"
-	if errStr != expectedStr {
-		t.Errorf("Incorrect errProjectNotFound error. Found: %s. Expected: %s", errStr, expectedStr)
+	if err != errProjectNotFound {
+		t.Errorf("findProjectRoot want: %#v got: %#v", errProjectNotFound, err)
 	}
 
-	// the following test does not work on windows because syscall.Stat does not
-	// return a "not a directory" error
+	// The following test does not work on windows because syscall.Stat does not
+	// return a "not a directory" error.
 	if runtime.GOOS != "windows" {
-		got4, err := findProjectRoot(filepath.Join(expect, manifestName))
+		got4, err := findProjectRoot(filepath.Join(want, manifestName))
 		if err == nil {
 			t.Errorf("Should have err'd when trying subdir of file, but returned %s", got4)
 		}
