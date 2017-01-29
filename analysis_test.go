27aff4dc3fdb53f5fecbdb736056db2c71d8eb4a
ReSTARTR <yoshida.masaki+restartr@gmail.com>
2017-01-29 15:00:29 +0900

    Fix syntax error

--- analysis_test.go
+++ analysis_test.go
@@ -917,6 +917,12 @@ func TestListPackages(t *testing.T) {
 							Imports: []string{
 								"github.com/sdboyer/gps",
 								"symlinks/gopkg",
+							},
+						},
+					},
+				},
+			},
+		},
 		"invalid buildtag like comments should be ignored": {
 			fileRoot:   j("buildtag"),
 			importRoot: "buildtag",
