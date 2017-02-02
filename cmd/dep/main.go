9639c033042cff9826b6300418d2958dccc4a8b7
Victor Vrantchan <vrancean+github@gmail.com>
2017-02-02 11:14:18 -0500

    improve support for printing command usage
    Allow the user to use "-h" or "help" before a command name to print it's usage.
    Closes #211

--- cmd/dep/main.go
+++ cmd/dep/main.go
@@ -55,13 +55,44 @@ func main() {
 		fmt.Fprintln(os.Stderr)
 	}
 
-	if len(os.Args) <= 1 || len(os.Args) == 2 && (strings.Contains(strings.ToLower(os.Args[1]), "help") || strings.ToLower(os.Args[1]) == "-h") {
+	// parseArgs determines the name of the dep command and wether the user asked for
+	// help to be printed.
+	parseArgs := func(args []string) (cmdName string, printCmdUsage bool, exit bool) {
+		isHelpArg := func() bool {
+			if strings.Contains(strings.ToLower(args[1]), "help") ||
+				strings.ToLower(args[1]) == "-h" {
+				return true
+			}
+			return false
+		}
+
+		switch len(args) {
+		case 0, 1:
+			exit = true
+		case 2:
+			if isHelpArg() {
+				exit = true
+			}
+			cmdName = args[1]
+		default:
+			if isHelpArg() {
+				cmdName = args[2]
+				printCmdUsage = true
+			} else {
+				cmdName = args[1]
+			}
+		}
+		return cmdName, printCmdUsage, exit
+	}
+
+	cmdName, printCommandHelp, exit := parseArgs(os.Args)
+	if exit {
 		usage()
 		os.Exit(1)
 	}
 
 	for _, cmd := range commands {
-		if name := cmd.Name(); os.Args[1] == name {
+		if name := cmd.Name(); cmdName == name {
 			// Build flag set with global flags in there.
 			// TODO(pb): can we deglobalize verbose, pretty please?
 			fs := flag.NewFlagSet(name, flag.ExitOnError)
@@ -72,6 +103,11 @@ func main() {
 
 			// Override the usage text to something nicer.
 			resetUsage(fs, cmd.Name(), cmd.Args(), cmd.LongHelp())
+
+			if printCommandHelp {
+				fs.Usage()
+				os.Exit(1)
+			}
 
 			// Parse the flags the user gave us.
 			if err := fs.Parse(os.Args[2:]); err != nil {
