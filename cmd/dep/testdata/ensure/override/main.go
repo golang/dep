355d7dc3bede4016a1ad69cd45f87e619ee17d9c
tro3 <trey.roessig@gmail.com>
2017-03-03 10:04:18 -0800

    Gofmt tweaks to pass Travis

--- cmd/dep/testdata/ensure/override/main.go
+++ cmd/dep/testdata/ensure/override/main.go
@@ -1,12 +1,13 @@
-// Copyright 2016 The Go Authors. All rights reserved.
+// Copyright 2017 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
 package main
 
 import (
-  "fmt"
-  stuff "github.com/sdboyer/deptest"
+	"fmt"
+
+	stuff "github.com/sdboyer/deptest"
 )
 
 func main() {
