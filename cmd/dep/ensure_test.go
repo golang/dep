5e0704f1f29b5ea779df84db26edd9d26f40fbbf
tro3 <trey.roessig@gmail.com>
2017-03-05 19:19:58 -0800

    Remove old tests and promote new hierarchy one level

--- cmd/dep/ensure_test.go
+++ cmd/dep/ensure_test.go
@@ -5,10 +5,8 @@
 package main
 
 import (
-	"path/filepath"
 	"testing"
 
-	"github.com/golang/dep/test"
 	"github.com/sdboyer/gps"
 )
 
@@ -36,111 +34,5 @@ func TestDeduceConstraint(t *testing.T) {
 		if c != expected {
 			t.Fatalf("expected: %#v, got %#v for %s", expected, c, str)
 		}
-	}
-}
-
-type ensureTestCase struct {
-	dataRoot       string
-	commands       [][]string
-	sourceFiles    map[string]string
-	goldenManifest string
-	goldenLock     string
-}
-
-func TestEnsureCases(t *testing.T) {
-	tests := []ensureTestCase{
-
-		// Override test case
-		{
-			dataRoot: "ensure/override",
-			commands: [][]string{
-				{"init"},
-				{"ensure", "-override", "github.com/sdboyer/deptest@1.0.0"},
-			},
-			sourceFiles: map[string]string{
-				"main.go": "thing.go",
-			},
-			goldenManifest: "manifest.golden.json",
-			goldenLock:     "lock.golden.json",
-		},
-
-		// Empty repo test case
-		{
-			dataRoot: "ensure/empty",
-			commands: [][]string{
-				{"init"},
-				{"ensure"},
-			},
-			sourceFiles: map[string]string{
-				"main.go": "thing.go",
-			},
-			goldenManifest: "manifest.golden.json",
-			goldenLock:     "lock.golden.json",
-		},
-
-		// Update test case
-		{
-			dataRoot: "ensure/update",
-			commands: [][]string{
-				{"ensure", "-update", "github.com/sdboyer/deptest"},
-			},
-			sourceFiles: map[string]string{
-				"main.go":       "thing.go",
-				"manifest.json": "manifest.json",
-				"lock.json":     "lock.json",
-			},
-			goldenManifest: "manifest.json",
-			goldenLock:     "lock.golden.json",
-		},
-	}
-
-	test.NeedsExternalNetwork(t)
-	test.NeedsGit(t)
-
-	for _, testCase := range tests {
-		t.Run(testCase.dataRoot, func(t *testing.T) {
-			h := test.NewHelper(t)
-			defer h.Cleanup()
-
-			h.TempDir("src")
-			h.Setenv("GOPATH", h.Path("."))
-
-			// Build a fake consumer of these packages.
-			root := "src/thing"
-			for src, dest := range testCase.sourceFiles {
-				h.TempCopy(filepath.Join(root, dest), filepath.Join(testCase.dataRoot, src))
-			}
-			h.Cd(h.Path(root))
-
-			for _, cmd := range testCase.commands {
-				h.Run(cmd...)
-			}
-
-			wantPath := filepath.Join(testCase.dataRoot, testCase.goldenManifest)
-			wantManifest := h.GetTestFileString(wantPath)
-			gotManifest := h.ReadManifest()
-			if wantManifest != gotManifest {
-				if *test.UpdateGolden {
-					if err := h.WriteTestFile(wantPath, gotManifest); err != nil {
-						t.Fatal(err)
-					}
-				} else {
-					t.Errorf("expected %s, got %s", wantManifest, gotManifest)
-				}
-			}
-
-			wantPath = filepath.Join(testCase.dataRoot, testCase.goldenLock)
-			wantLock := h.GetTestFileString(wantPath)
-			gotLock := h.ReadLock()
-			if wantLock != gotLock {
-				if *test.UpdateGolden {
-					if err := h.WriteTestFile(wantPath, gotLock); err != nil {
-						t.Fatal(err)
-					}
-				} else {
-					t.Errorf("expected %s, got %s", wantLock, gotLock)
-				}
-			}
-		})
 	}
 }
