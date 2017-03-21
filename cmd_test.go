ec452354dafa6a71361623fa9a4d95e2ec6ae2d2
Spencer Nelson <s@spenczar.com>
2017-03-21 16:19:34 -0400

    Give tested monitored cmd a little slack

--- cmd_test.go
+++ cmd_test.go
@@ -11,7 +11,7 @@ import (
 func mkTestCmd(iterations int) *monitoredCmd {
 	return newMonitoredCmd(
 		exec.Command("./echosleep", "-n", fmt.Sprint(iterations)),
-		200*time.Millisecond,
+		(time.Duration(iterations+1))*100*time.Millisecond,
 	)
 }
 
