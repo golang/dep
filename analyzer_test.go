12e713b5463096493f4b43b1521d11f73c660388
Carolyn Van Slyck <me@carolynvanslyck.com>
2017-03-21 11:23:53 -0500

    Update test files from JSON to TOML

--- analyzer_test.go
+++ analyzer_test.go
@@ -18,7 +18,7 @@ func TestDeriveManifestAndLock(t *testing.T) {
 	defer h.Cleanup()
 
 	h.TempDir("dep")
-	golden := "analyzer/manifest.json"
+	golden := "analyzer/manifest.toml"
 	want := h.GetTestFileString(golden)
 	h.TempCopy(filepath.Join("dep", ManifestName), golden)
 
