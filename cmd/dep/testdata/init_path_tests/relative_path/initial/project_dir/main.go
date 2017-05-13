4d667198aed87cc3e74df3687ee13e9bf0e50ea9
Niranjan Godbole <niranjan8192@gmail.com>
2017-05-14 04:35:12 +0530

    dep init adds test cases for relative path

---
+++ cmd/dep/testdata/init_path_tests/relative_path/initial/project_dir/main.go
@@ -0,0 +1,18 @@
+// Copyright 2017 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"fmt"
+
+	"github.com/sdboyer/deptestdos"
+)
+
+func main() {
+	var x deptestdos.Bar
+	y := foo.FooFunc()
+
+	fmt.Println(x, y)
+}
