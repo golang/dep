ec237f2afef4d13a04853967fb98b7a65aaaf1a9
Ibrahim AshShohail <ibra.sho@gmail.com>
2017-05-16 12:14:50 +0300

    internal/fs: fix bugs in windows and fs
    Signed-off-by: Ibrahim AshShohail <ibra.sho@gmail.com>

--- internal/fs/rename_windows.go
+++ internal/fs/rename_windows.go
@@ -19,7 +19,7 @@ func rename(src, dst string) error {
 
 // renameFallback attempts to determine the appropriate fallback to failed rename
 // operation depending on the resulting error.
-func renameFallback(src, dst string) error {
+func renameFallback(err error, src, dst string) error {
 	// Rename may fail if src and dst are on different devices; fall back to
 	// copy if we detect that case. syscall.EXDEV is the common name for the
 	// cross device link error which has varying output text across different
