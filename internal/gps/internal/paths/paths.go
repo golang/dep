1e9241654a8593e1bf3445b573599c63eb2d549e
Jordan Krage <jmank88@gmail.com>
2017-05-17 20:37:25 -0500

    better IsStandardImportPath doc

--- internal/gps/internal/paths/paths.go
+++ internal/gps/internal/paths/paths.go
@@ -6,6 +6,9 @@ package paths
 
 import "strings"
 
+// IsStandardImportPath indicates whether the provided path should be considered
+// part of the standard library by checking the first path element for a '.'.
+//
 // This was lovingly lifted from src/cmd/go/pkg.go in Go's code.
 func IsStandardImportPath(path string) bool {
 	i := strings.Index(path, "/")
