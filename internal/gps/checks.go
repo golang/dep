0934d44d37e14f364070641d4c482fe5b4122dcb
Milutin Jovanović <jovanovic.milutin@gmail.com>
2017-06-12 08:13:59 -0400

    release candidate

--- internal/gps/checks.go
+++ internal/gps/checks.go
@@ -8,70 +8,32 @@ import (
 	"github.com/golang/dep/internal/gps/pkgtree"
 )
 
-func FindIneffectualConstraints(manifest Manifest, packageTree pkgtree.PackageTree) ProjectConstraints {
-	// Merge the normal and test constraints together
-	constraints := manifest.DependencyConstraints().merge(manifest.TestDependencyConstraints())
-
-	// var workingConstraints []workingConstraint
-	// var ignoredPackages map[string]bool
-	// var requiredPackages map[string]bool
-	// if manifest is a RootManifest, then apply the overrides
+func FindIneffectualConstraints(manifest Manifest, packageTree pkgtree.PackageTree) []ProjectRoot {
+	// gather all constraints which should be checked
+	constraints := make(map[ProjectRoot]bool)
+	for projectRoot, _ := range manifest.DependencyConstraints() {
+		constraints[projectRoot] = true
+	}
+	for projectRoot, _ := range manifest.TestDependencyConstraints() {
+		constraints[projectRoot] = true
+	}
+	// if manifest is a RootManifest, check requires and ignores
 	if rootManifest := manifest.(RootManifest); rootManifest != nil {
-		// Include any overrides that are not already in the constraint list.
-		// Doing so makes input hashes equal in more useful cases.
-		for projectRoot, projectProperties := range rootManifest.Overrides() {
-			if _, exists := constraints[projectRoot]; !exists {
-				pp := ProjectProperties{
-					Constraint: projectProperties.Constraint,
-					Source:     projectProperties.Source,
-				}
-				if pp.Constraint == nil {
-					pp.Constraint = anyConstraint{}
-				}
-
-				constraints[projectRoot] = pp
-			}
+		for projectRoot, _ := range rootManifest.RequiredPackages() {
+			constraints[ProjectRoot(projectRoot)] = true
 		}
-
-		// ignoredPackages = rootManifest.IgnoredPackages()
-		// requiredPackages = rootManifest.RequiredPackages()
+		for projectRoot, _ := range rootManifest.IgnoredPackages() {
+			constraints[ProjectRoot(projectRoot)] = true
+		}
 	}
 
-	var ineffectuals ProjectConstraints
-	for packageRoot, packageProperties := range constraints {
-		if _, used := packageTree.Packages[string(packageRoot)]; !used {
-			ineffectuals[packageRoot] = packageProperties
+	// now check the constraints against the packageTree
+	ineffectuals := make([]ProjectRoot, 0, 10)
+	for projectRoot, _ := range constraints {
+		if _, used := packageTree.Packages[string(projectRoot)]; !used {
+			ineffectuals = append(ineffectuals, projectRoot)
 		}
 	}
 
 	return ineffectuals
 }
-
-// // externalImportList returns a list of the unique imports from the root data.
-// // Ignores and requires are taken into consideration, stdlib is excluded, and
-// // errors within the local set of package are not backpropagated.
-// func externalImportList2(packageTree pkgtree.PackageTree, requiredPackages, ignoredPackages map[string]bool, stdLibFn func(string) bool) []string {
-// 	reachMap, _ := packageTree.ToReachMap(true, true, false, ignoredPackages)
-// 	reach := reachMap.FlattenFn(stdLibFn)
-//
-// 	// If there are any requires, slide them into the reach list, as well.
-// 	if len(requiredPackages) > 0 {
-// 		// Make a map of imports that are both in the import path list and the
-// 		// required list to avoid duplication.
-// 		skip := make(map[string]bool, len(requiredPackages))
-// 		for _, r := range reach {
-// 			if requiredPackages[r] {
-// 				skip[r] = true
-// 			}
-// 		}
-//
-// 		for r := range requiredPackages {
-// 			if !skip[r] {
-// 				reach = append(reach, r)
-// 			}
-// 		}
-// 	}
-//
-// 	sort.Strings(reach)
-// 	return reach
-// }
