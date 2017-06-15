3ce19cd753d8350ab6fc7dcd8dca6d1ffa92f3f9
sam boyer <tech@samboyer.org>
2017-06-14 23:27:13 -0400

    Tests for ensure flag/arg validation

--- cmd/dep/ensure_test.go
+++ cmd/dep/ensure_test.go
@@ -74,3 +74,43 @@ func TestDeduceConstraint_InvalidInput(t *testing.T) {
 		}
 	}
 }
+
+func TestInvalidEnsureFlagCombinations(t *testing.T) {
+	ec := &ensureCommand{
+		update: true,
+		add:    true,
+	}
+
+	if err := ec.validateFlags(); err == nil {
+		t.Error("-add and -update together should fail validation")
+	}
+
+	ec.vendorOnly, ec.add = true, false
+	if err := ec.validateFlags(); err == nil {
+		t.Error("-vendor-only with -update should fail validation")
+	}
+
+	ec.add, ec.update = true, false
+	if err := ec.validateFlags(); err == nil {
+		t.Error("-vendor-only with -add should fail validation")
+	}
+
+	ec.noVendor, ec.add = true, false
+	if err := ec.validateFlags(); err == nil {
+		t.Error("-vendor-only with -no-vendor should fail validation")
+	}
+	ec.noVendor = false
+
+	// Also verify that the plain ensure path takes no args. This is a shady
+	// test, as lots of other things COULD return errors, and we don't check
+	// anything other than the error being non-nil. For now, it works well
+	// because a panic will quickly result if the initial arg length validation
+	// checks are incorrectly handled.
+	if err := ec.runDefault(nil, []string{"foo"}, nil, nil, gps.SolveParameters{}); err == nil {
+		t.Errorf("no args to plain ensure with -vendor-only")
+	}
+	ec.vendorOnly = false
+	if err := ec.runDefault(nil, []string{"foo"}, nil, nil, gps.SolveParameters{}); err == nil {
+		t.Errorf("no args to plain ensure")
+	}
+}
