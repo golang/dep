249fc211414694fa8b57bfff8c1c77ccda8c1f64
Alex Vidal <alex.vidal@gmail.com>
2017-07-15 14:21:08 -0600

    fix(gps): support source urls that point directly to go.googlesource.com
    This fixes a bug which can be reproduced simply:
    
    ```
    $ dep ensure golang.org/x/crypto:go.googlesource.com/crypto
    list versions for golang.org/x/crypto(https://go.googlesource.com/crypto): unable to deduce repository and source type for "https://go.googlesource.com/crypto": unable to read metadata: go-import metadata not found
    ```
    
    The problem comes up because there was no deducer for
    go.googlesource.com, and go.googlesource.com itself does not have any
    metatags that can be used to deduce the vcs.
    
    Generally speaking, this bug would not come up because most users would
    target golang.org/x/crypto, which *does* have all of the required
    metadata, but it's a bug all the same.

--- internal/gps/deduce.go
+++ internal/gps/deduce.go
@@ -72,6 +72,7 @@ var (
 	//gcRegex      = regexp.MustCompile(`^(?P<root>code\.google\.com/[pr]/(?P<project>[a-z0-9\-]+)(\.(?P<subrepo>[a-z0-9\-]+))?)(/[A-Za-z0-9_.\-]+)*$`)
 	jazzRegex         = regexp.MustCompile(`^(?P<root>hub\.jazz\.net(/git/[a-z0-9]+/[A-Za-z0-9_.\-]+))((?:/[A-Za-z0-9_.\-]+)*)$`)
 	apacheRegex       = regexp.MustCompile(`^(?P<root>git\.apache\.org(/[a-z0-9_.\-]+\.git))((?:/[A-Za-z0-9_.\-]+)*)$`)
+	goSourceRegex     = regexp.MustCompile(`^(?P<root>go\.googlesource\.com(/[A-Za-z0-9-._]+))((?:/[A-Za-z0-9_.\-]+)*)$`)
 	vcsExtensionRegex = regexp.MustCompile(`^(?P<root>([a-z0-9.\-]+\.)+[a-z0-9.\-]+(:[0-9]+)?/[A-Za-z0-9_.\-/~]*?\.(?P<vcs>bzr|git|hg|svn))((?:/[A-Za-z0-9_.\-]+)*)$`)
 )
 
@@ -91,6 +92,7 @@ func pathDeducerTrie() *deducerTrie {
 	dxt.Insert("git.launchpad.net/", launchpadGitDeducer{regexp: glpRegex})
 	dxt.Insert("hub.jazz.net/", jazzDeducer{regexp: jazzRegex})
 	dxt.Insert("git.apache.org/", apacheDeducer{regexp: apacheRegex})
+	dxt.Insert("go.googlesource.com/", goSourceDeducer{regexp: goSourceRegex})
 
 	return dxt
 }
@@ -461,6 +463,39 @@ func (m apacheDeducer) deduceSource(path string, u *url.URL) (maybeSource, error
 	}
 
 	return mb, nil
+}
+
+type goSourceDeducer struct {
+	regexp *regexp.Regexp
+}
+
+func (m goSourceDeducer) deduceRoot(path string) (string, error) {
+	v := m.regexp.FindStringSubmatch(path)
+	if v == nil {
+		return "", fmt.Errorf("%s is not a valid path for a source on go.googlesource.com", path)
+	}
+
+	return "go.googlesource.com" + v[2], nil
+}
+
+func (m goSourceDeducer) deduceSource(path string, u *url.URL) (maybeSource, error) {
+	v := m.regexp.FindStringSubmatch(path)
+	if v == nil {
+		return nil, fmt.Errorf("%s is not a valid path for a source on go.googlesource.com", path)
+	}
+
+	u.Host = "go.googlesource.com"
+	u.Path = v[2]
+
+	switch u.Scheme {
+	case "":
+		u.Scheme = "https"
+		fallthrough
+	case "https":
+		return maybeGitSource{url: u}, nil
+	default:
+		return nil, fmt.Errorf("go.googlesource.com only supports https, %s is not allowed", u.String())
+	}
 }
 
 type vcsExtensionDeducer struct {
