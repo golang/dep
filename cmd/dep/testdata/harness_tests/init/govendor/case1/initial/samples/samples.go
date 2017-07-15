e30f0271aca7e3a9da7bc220b876c67e5bdc30f2
Brendan Munro <brendan.munro@whiteops.com>
2017-07-15 12:55:54 -0700

    internal/importers: add govendor importer
    Add vendor.json examples for govendor corner cases.
    
    Add first integration test case

---
+++ cmd/dep/testdata/harness_tests/init/govendor/case1/initial/samples/samples.go
@@ -0,0 +1,12 @@
+// Copyright 2016 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package samples
+
+import dt "github.com/carolynvs/go-dep-test"
+
+func Sample1() int {
+	var x = dt.Thing
+	return x
+}
