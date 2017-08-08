086098bc4af4863735b6dd35365a20cf46feff1f
Karrick S. McDermott <karrick@karrick.net>
2017-08-08 10:30:20 -0400

    lineEndingReader is more simplified than lineEndingWriterTo

--- internal/gps/pkgtree/digest_test.go
+++ internal/gps/pkgtree/digest_test.go
@@ -12,6 +12,67 @@ import (
 	"testing"
 )
 
+// crossBuffer is a test io.Reader that emits a few canned responses.
+type crossBuffer struct {
+	readCount int
+}
+
+func (cb *crossBuffer) Read(buf []byte) (int, error) {
+	const first = "this is the result\r\nfrom the first read\r"
+	const second = "\nthis is the result\r\nfrom the second read\r"
+
+	cb.readCount++
+
+	switch cb.readCount {
+	case 1:
+		return copy(buf, first), nil
+	case 2:
+		return copy(buf, second), nil
+	default:
+		return 0, io.EOF
+	}
+}
+
+////////////////////////////////////////
+
+func TestLineEndingReadCrossBufferCRLF(t *testing.T) {
+	// The final CR byte ought to be conveyed to destination.
+	const output = "this is the result\nfrom the first read\nthis is the result\nfrom the second read\r"
+
+	src := newLineEndingReader(new(crossBuffer))
+	dst := new(bytes.Buffer)
+
+	n, err := io.Copy(dst, src)
+	if got, want := err, error(nil); got != want {
+		t.Errorf("(GOT): %v; (WNT): %v", got, want)
+	}
+	if got, want := n, int64(len(output)); got != want {
+		t.Errorf("(GOT): %v; (WNT): %v", got, want)
+	}
+	if got, want := dst.Bytes(), []byte(output); !bytes.Equal(got, want) {
+		t.Errorf("(GOT): %#q; (WNT): %#q", got, want)
+	}
+}
+
+func TestLineEndingRead(t *testing.T) {
+	const input = "now is the time\r\nfor all good engineers\r\nto improve their test coverage!\r\n"
+	const output = "now is the time\nfor all good engineers\nto improve their test coverage!\n"
+
+	src := newLineEndingReader(bytes.NewBufferString(input))
+	dst := new(bytes.Buffer)
+
+	n, err := io.Copy(dst, src)
+	if got, want := err, error(nil); got != want {
+		t.Errorf("(GOT): %v; (WNT): %v", got, want)
+	}
+	if got, want := n, int64(len(output)); got != want {
+		t.Errorf("(GOT): %v; (WNT): %v", got, want)
+	}
+	if got, want := dst.Bytes(), []byte(output); !bytes.Equal(got, want) {
+		t.Errorf("(GOT): %#q; (WNT): %#q", got, want)
+	}
+}
+
 func getTestdataVerifyRoot(t *testing.T) string {
 	cwd, err := os.Getwd()
 	if err != nil {
@@ -23,7 +84,7 @@ func getTestdataVerifyRoot(t *testing.T) string {
 
 func TestDigestFromPathnameWithFile(t *testing.T) {
 	relativePathname := "github.com/alice/match/match.go"
-	want := []byte{0xef, 0x1e, 0x30, 0x16, 0xc4, 0xb1, 0xdb, 0xd9, 0x38, 0x65, 0xec, 0x90, 0xca, 0xad, 0x89, 0x52, 0xf9, 0x56, 0xb5, 0xa7, 0xfd, 0x83, 0x8e, 0xd7, 0x21, 0x6b, 0xbd, 0x63, 0x9a, 0xde, 0xc3, 0xeb}
+	want := []byte{0x43, 0xe6, 0x3, 0x53, 0xda, 0x4, 0x18, 0xf6, 0xbd, 0x98, 0xf4, 0x6c, 0x6d, 0xb8, 0xc1, 0x8d, 0xa2, 0x78, 0x16, 0x45, 0xf7, 0xca, 0xc, 0xec, 0xcf, 0x2e, 0xa1, 0x64, 0x55, 0x69, 0xbf, 0x8f}
 
 	// NOTE: Create the hash using both an absolute and a relative pathname to
 	// ensure hash ignores prefix.
@@ -53,7 +114,7 @@ func TestDigestFromPathnameWithFile(t *testing.T) {
 
 func TestDigestFromPathnameWithDirectory(t *testing.T) {
 	relativePathname := "launchpad.net/match"
-	want := []byte{0x8, 0xe5, 0x5b, 0xb6, 0xb8, 0x44, 0x91, 0x80, 0x46, 0xca, 0xc6, 0x2e, 0x44, 0x7b, 0x42, 0xd4, 0xfb, 0x2d, 0xfd, 0x4c, 0xd9, 0xc9, 0xd, 0x38, 0x23, 0xed, 0xa5, 0xf4, 0xbc, 0x69, 0xd4, 0x8b}
+	want := []byte{0x74, 0xe, 0x17, 0x87, 0xd4, 0x8e, 0x56, 0x2e, 0x7e, 0x32, 0x4e, 0x80, 0x3a, 0x5f, 0x3a, 0x10, 0x33, 0x43, 0x2c, 0x24, 0x8e, 0xf7, 0x1a, 0x37, 0x5e, 0x76, 0xf4, 0x6, 0x2b, 0xf3, 0xfd, 0x91}
 
 	// NOTE: Create the hash using both an absolute and a relative pathname to
 	// ensure hash ignores prefix.
@@ -85,13 +146,13 @@ func TestVerifyDepTree(t *testing.T) {
 	vendorRoot := getTestdataVerifyRoot(t)
 
 	wantSums := map[string][]byte{
-		"github.com/alice/match":       []byte{0x13, 0x22, 0x4c, 0xd5, 0x5b, 0x3e, 0x29, 0xe5, 0x40, 0x23, 0x56, 0xc7, 0xc1, 0x50, 0x4, 0x1, 0x60, 0x46, 0xd4, 0x14, 0x9b, 0x63, 0x88, 0x37, 0xeb, 0x53, 0xfc, 0xe7, 0x63, 0x53, 0xf3, 0xf6},
+		"github.com/alice/match":       []byte{0x87, 0x4, 0x53, 0x5f, 0xb8, 0xca, 0xe9, 0x52, 0x40, 0x41, 0xcd, 0x93, 0x2e, 0x42, 0xfb, 0x14, 0x77, 0x57, 0x9c, 0x3, 0x6b, 0xe1, 0x15, 0xe7, 0xfa, 0xc1, 0xf0, 0x98, 0x4b, 0x61, 0x9f, 0x48},
 		"github.com/alice/mismatch":    []byte("some non-matching digest"),
 		"github.com/bob/emptyDigest":   nil, // empty hash result
+		"github.com/bob/match":         []byte{0x6a, 0x11, 0xf9, 0x46, 0xcc, 0xf3, 0x44, 0xdb, 0x2c, 0x6b, 0xcc, 0xb4, 0x0, 0x71, 0xe5, 0xc4, 0xee, 0x9a, 0x26, 0x71, 0x9d, 0xab, 0xe2, 0x40, 0xb7, 0xbf, 0x2a, 0xd9, 0x4, 0xcf, 0xc9, 0x46},
 		"github.com/charlie/notInTree": nil, // not in tree result ought to superseede empty digest result
-		"github.com/bob/match":         []byte{0x73, 0xf5, 0xdc, 0xad, 0x39, 0x31, 0x25, 0x78, 0x9d, 0x2c, 0xbe, 0xd0, 0x9e, 0x6, 0x40, 0x64, 0xcd, 0x32, 0x9c, 0x28, 0xf4, 0xa0, 0xd2, 0xa4, 0x46, 0xca, 0x1f, 0x13, 0xfb, 0xaf, 0x53, 0x96},
 		// matching result at seldomly found directory level
-		"launchpad.net/match": []byte{0x8, 0xe5, 0x5b, 0xb6, 0xb8, 0x44, 0x91, 0x80, 0x46, 0xca, 0xc6, 0x2e, 0x44, 0x7b, 0x42, 0xd4, 0xfb, 0x2d, 0xfd, 0x4c, 0xd9, 0xc9, 0xd, 0x38, 0x23, 0xed, 0xa5, 0xf4, 0xbc, 0x69, 0xd4, 0x8b},
+		"launchpad.net/match": []byte{0x74, 0xe, 0x17, 0x87, 0xd4, 0x8e, 0x56, 0x2e, 0x7e, 0x32, 0x4e, 0x80, 0x3a, 0x5f, 0x3a, 0x10, 0x33, 0x43, 0x2c, 0x24, 0x8e, 0xf7, 0x1a, 0x37, 0x5e, 0x76, 0xf4, 0x6, 0x2b, 0xf3, 0xfd, 0x91},
 	}
 
 	status, err := VerifyDepTree(vendorRoot, wantSums)
@@ -147,63 +208,5 @@ func BenchmarkVerifyDepTree(b *testing.B) {
 		if err != nil {
 			b.Fatal(err)
 		}
-	}
-}
-
-type crossBuffer struct {
-	readCount int
-}
-
-func (cb *crossBuffer) Read(buf []byte) (int, error) {
-	const first = "this is the result\r\nfrom the first read\r"
-	const second = "\nthis is the result\r\nfrom the second read\r"
-
-	cb.readCount++
-
-	switch cb.readCount {
-	case 1:
-		return copy(buf, first), nil
-	case 2:
-		return copy(buf, second), nil
-	default:
-		return 0, io.EOF
-	}
-}
-
-func TestLineEndingWriteToCrossBufferCRLF(t *testing.T) {
-	src := &lineEndingWriterTo{new(crossBuffer)}
-	dst := new(bytes.Buffer)
-
-	// the final CR byte ought to be conveyed to destination
-	const output = "this is the result\nfrom the first read\nthis is the result\nfrom the second read\r"
-
-	n, err := io.Copy(dst, src)
-	if got, want := err, error(nil); got != want {
-		t.Errorf("(GOT): %v; (WNT): %v", got, want)
-	}
-	if got, want := n, int64(len(output)); got != want {
-		t.Errorf("(GOT): %v; (WNT): %v", got, want)
-	}
-	if got, want := dst.Bytes(), []byte(output); !bytes.Equal(got, want) {
-		t.Errorf("(GOT): %#q; (WNT): %#q", got, want)
-	}
-}
-
-func TestLineEndingWriteTo(t *testing.T) {
-	const input = "now is the time\r\nfor all good engineers\r\nto improve their test coverage!\r\n"
-	const output = "now is the time\nfor all good engineers\nto improve their test coverage!\n"
-
-	src := &lineEndingWriterTo{bytes.NewBufferString(input)}
-	dst := new(bytes.Buffer)
-
-	n, err := io.Copy(dst, src)
-	if got, want := err, error(nil); got != want {
-		t.Errorf("(GOT): %v; (WNT): %v", got, want)
-	}
-	if got, want := n, int64(len(output)); got != want {
-		t.Errorf("(GOT): %v; (WNT): %v", got, want)
-	}
-	if got, want := dst.Bytes(), []byte(output); !bytes.Equal(got, want) {
-		t.Errorf("(GOT): %#q; (WNT): %#q", got, want)
 	}
 }
