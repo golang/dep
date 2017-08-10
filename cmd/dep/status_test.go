a530b2535eba0f194c82d95e4ba30f8fed8dcffa
Sunny <me@darkowlzz.space>
2017-08-10 18:17:19 +0530

    test(status): BasicLine output & BasicStatus func
    Add table tests for BasicLine output for each output type and tests for
    getConsolidatedConstraint() & getConsolidatedVersion().

--- cmd/dep/status_test.go
+++ cmd/dep/status_test.go
@@ -7,6 +7,7 @@ package main
 import (
 	"bytes"
 	"testing"
+	"text/tabwriter"
 
 	"strings"
 
@@ -34,34 +35,182 @@ func TestStatusFormatVersion(t *testing.T) {
 func TestBasicLine(t *testing.T) {
 
 	project := dep.Project{}
+	aSemverConstraint, _ := gps.NewSemverConstraint("1.2.3")
 
-	var tests = []struct {
-		status   BasicStatus
-		expected string
+	tests := []struct {
+		name            string
+		status          BasicStatus
+		wantDotStatus   []string
+		wantJSONStatus  []string
+		wantTableStatus []string
 	}{
-		{BasicStatus{
-			Version:  nil,
-			Revision: gps.Revision("flooboofoobooo"),
-		}, `[label="\nflooboo"];`},
-		{BasicStatus{
-			Version:  gps.NewVersion("1.0.0"),
-			Revision: gps.Revision("flooboofoobooo"),
-		}, `[label="\n1.0.0"];`},
+		{
+			name: "BasicStatus with ProjectRoot only",
+			status: BasicStatus{
+				ProjectRoot: "github.com/foo/bar",
+			},
+			wantDotStatus:   []string{`[label="github.com/foo/bar"];`},
+			wantJSONStatus:  []string{`"Version":""`, `"Revision":""`},
+			wantTableStatus: []string{`github.com/foo/bar                                         0`},
+		},
+		{
+			name: "BasicStatus with Revision",
+			status: BasicStatus{
+				ProjectRoot: "github.com/foo/bar",
+				Revision:    gps.Revision("flooboofoobooo"),
+			},
+			wantDotStatus:   []string{`[label="github.com/foo/bar\nflooboo"];`},
+			wantJSONStatus:  []string{`"Version":""`, `"Revision":"flooboofoobooo"`, `"Constraint":""`},
+			wantTableStatus: []string{`github.com/foo/bar                       flooboo           0`},
+		},
+		{
+			name: "BasicStatus with Version and Revision",
+			status: BasicStatus{
+				ProjectRoot: "github.com/foo/bar",
+				Version:     gps.NewVersion("1.0.0"),
+				Revision:    gps.Revision("flooboofoobooo"),
+			},
+			wantDotStatus:   []string{`[label="github.com/foo/bar\n1.0.0"];`},
+			wantJSONStatus:  []string{`"Version":"1.0.0"`, `"Revision":"flooboofoobooo"`, `"Constraint":""`},
+			wantTableStatus: []string{`github.com/foo/bar              1.0.0    flooboo           0`},
+		},
+		{
+			name: "BasicStatus with Constraint, Version and Revision",
+			status: BasicStatus{
+				ProjectRoot: "github.com/foo/bar",
+				Constraint:  aSemverConstraint,
+				Version:     gps.NewVersion("1.0.0"),
+				Revision:    gps.Revision("revxyz"),
+			},
+			wantDotStatus:   []string{`[label="github.com/foo/bar\n1.0.0"];`},
+			wantJSONStatus:  []string{`"Revision":"revxyz"`, `"Constraint":"1.2.3"`, `"Version":"1.0.0"`},
+			wantTableStatus: []string{`github.com/foo/bar  1.2.3       1.0.0    revxyz            0`},
+		},
 	}
 
 	for _, test := range tests {
-		var buf bytes.Buffer
+		t.Run(test.name, func(t *testing.T) {
+			var buf bytes.Buffer
 
-		out := &dotOutput{
-			p: &project,
-			w: &buf,
-		}
-		out.BasicHeader()
-		out.BasicLine(&test.status)
-		out.BasicFooter()
+			dotout := &dotOutput{
+				p: &project,
+				w: &buf,
+			}
+			dotout.BasicHeader()
+			dotout.BasicLine(&test.status)
+			dotout.BasicFooter()
 
-		if ok := strings.Contains(buf.String(), test.expected); !ok {
-			t.Fatalf("Did not find expected node label: \n\t(GOT) %v \n\t(WNT) %v", buf.String(), test.status)
-		}
+			for _, wantStatus := range test.wantDotStatus {
+				if ok := strings.Contains(buf.String(), wantStatus); !ok {
+					t.Errorf("Did not find expected node status: \n\t(GOT) %v \n\t(WNT) %v", buf.String(), wantStatus)
+				}
+			}
+
+			buf.Reset()
+
+			jsonout := &jsonOutput{w: &buf}
+
+			jsonout.BasicHeader()
+			jsonout.BasicLine(&test.status)
+			jsonout.BasicFooter()
+
+			for _, wantStatus := range test.wantJSONStatus {
+				if ok := strings.Contains(buf.String(), wantStatus); !ok {
+					t.Errorf("Did not find expected JSON status: \n\t(GOT) %v \n\t(WNT) %v", buf.String(), wantStatus)
+				}
+			}
+
+			buf.Reset()
+
+			tabw := tabwriter.NewWriter(&buf, 0, 4, 2, ' ', 0)
+
+			tableout := &tableOutput{w: tabw}
+
+			tableout.BasicHeader()
+			tableout.BasicLine(&test.status)
+			tableout.BasicFooter()
+
+			for _, wantStatus := range test.wantTableStatus {
+				if ok := strings.Contains(buf.String(), wantStatus); !ok {
+					t.Errorf("Did not find expected Table status: \n\t(GOT) %v \n\t(WNT) %v", buf.String(), wantStatus)
+				}
+			}
+		})
+	}
+}
+
+func TestBasicStatusGetConsolidatedConstraint(t *testing.T) {
+
+	aSemverConstraint, _ := gps.NewSemverConstraint("1.2.1")
+
+	testCases := []struct {
+		name           string
+		basicStatus    BasicStatus
+		wantConstraint string
+	}{
+		{
+			name:           "empty BasicStatus",
+			basicStatus:    BasicStatus{},
+			wantConstraint: "",
+		},
+		{
+			name: "BasicStatus with Any Constraint",
+			basicStatus: BasicStatus{
+				Constraint: gps.Any(),
+			},
+			wantConstraint: "*",
+		},
+		{
+			name: "BasicStatus with Semver Constraint",
+			basicStatus: BasicStatus{
+				Constraint: aSemverConstraint,
+			},
+			wantConstraint: "1.2.1",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.basicStatus.getConsolidatedConstraint() != tc.wantConstraint {
+				t.Errorf("unexpected consolidated constraint: \n\t(GOT) %v \n\t(WNT) %v", tc.basicStatus.getConsolidatedConstraint(), tc.wantConstraint)
+			}
+		})
+	}
+}
+
+func TestBasicStatusGetConsolidatedVersion(t *testing.T) {
+	testCases := []struct {
+		name        string
+		basicStatus BasicStatus
+		wantVersion string
+	}{
+		{
+			name:        "empty BasicStatus",
+			basicStatus: BasicStatus{},
+			wantVersion: "",
+		},
+		{
+			name: "BasicStatus with Version and Revision",
+			basicStatus: BasicStatus{
+				Version:  gps.NewVersion("1.0.0"),
+				Revision: gps.Revision("revxyz"),
+			},
+			wantVersion: "1.0.0",
+		},
+		{
+			name: "BasicStatus with only Revision",
+			basicStatus: BasicStatus{
+				Revision: gps.Revision("revxyz"),
+			},
+			wantVersion: "revxyz",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.basicStatus.getConsolidatedVersion() != tc.wantVersion {
+				t.Errorf("unexpected consolidated version: \n\t(GOT) %v \n\t(WNT) %v", tc.basicStatus.getConsolidatedVersion(), tc.wantVersion)
+			}
+		})
 	}
 }
