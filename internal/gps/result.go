b90c0e90746ea1b2e82c0f76855407768fd19deb
Kashav Madan <kshvmdn@gmail.com>
2017-08-12 23:29:44 -0400

    Verify logger is non-nil in WriteDepTree
    Fixes #1003.

--- internal/gps/result.go
+++ internal/gps/result.go
@@ -66,7 +66,9 @@ func WriteDepTree(basedir string, l Lock, sm SourceManager, sv bool, logger *log
 	for _, p := range l.Projects() {
 		to := filepath.FromSlash(filepath.Join(basedir, string(p.Ident().ProjectRoot)))
 
-		logger.Printf("Writing out %s@%s", p.Ident().errString(), p.Version())
+		if logger != nil {
+			logger.Printf("Writing out %s@%s", p.Ident().errString(), p.Version())
+		}
 		err = sm.ExportProject(p.Ident(), p.Version(), to)
 		if err != nil {
 			removeAll(basedir)
