9e91480facec9f583a86d1c01d1053dd07a6aa93
Dave Josephsen <dave.josephsen@sparkpost.com>
2017-08-14 16:00:30 -0500

    #907 is this sane?

--- cmd/dep/vndr_importer.go
+++ cmd/dep/vndr_importer.go
@@ -106,7 +106,8 @@ func (v *vndrImporter) convert(pr gps.ProjectRoot) (*dep.Manifest, *dep.Lock, er
 		}
 		pc.Constraint, err = v.sm.InferConstraint(pkg.revision, pc.Ident)
 		if err != nil {
-			return nil, nil, errors.Wrapf(err, "Unable to interpret revision specifier '%s' for package %s", pkg.importPath, pkg.revision)
+			v.logger.Printf("Unable to interpret revision specifier '%s' for package %s: %s", pkg.importPath, pkg.revision, err.Error())
+			continue
 		}
 
 		manifest.Constraints[pc.Ident.ProjectRoot] = gps.ProjectProperties{
@@ -127,7 +128,11 @@ func (v *vndrImporter) convert(pr gps.ProjectRoot) (*dep.Manifest, *dep.Lock, er
 		fb.NewLockedProjectFeedback(lp, fb.DepTypeImported).LogFeedback(v.logger)
 	}
 
-	return manifest, lock, nil
+	if len(manifest.Constraints) > 0 {
+		return manifest, lock, nil
+	} else {
+		return nil, nil, fmt.ErrorF("No enforcable constraints found")
+	}
 }
 
 type vndrPackage struct {
