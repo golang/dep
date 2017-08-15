e9ff6b28248e6ce3b65315bfa1a21da09074937a
Karrick S. McDermott <karrick@karrick.net>
2017-08-14 22:15:01 -0500

    additional test cases

--- internal/gps/pkgtree/digest_test.go
+++ internal/gps/pkgtree/digest_test.go
@@ -52,6 +52,28 @@ func TestLineEndingReader(t *testing.T) {
 		{[]string{"\rnow is the time"}, "\rnow is the time"},     // CR not followed by LF ought to convey
 		{[]string{"\rnow is the time\r"}, "\rnow is the time\r"}, // CR not followed by LF ought to convey
 
+		// no line splits
+		{[]string{"first", "second", "third"}, "firstsecondthird"},
+
+		// 1->2 and 2->3 both break across a CRLF
+		{[]string{"first\r", "\nsecond\r", "\nthird"}, "first\nsecond\nthird"},
+
+		// 1->2 breaks across CRLF and 2->3 does not
+		{[]string{"first\r", "\nsecond", "third"}, "first\nsecondthird"},
+
+		// 1->2 breaks across CRLF and 2 ends in CR but 3 does not begin LF
+		{[]string{"first\r", "\nsecond\r", "third"}, "first\nsecond\rthird"},
+
+		// 1 ends in CR but 2 does not begin LF, and 2->3 breaks across CRLF
+		{[]string{"first\r", "second\r", "\nthird"}, "first\rsecond\nthird"},
+
+		// 1 ends in CR but 2 does not begin LF, and 2->3 does not break across CRLF
+		{[]string{"first\r", "second\r", "\nthird"}, "first\rsecond\nthird"},
+
+		// 1->2 and 2->3 both break across a CRLF, but 3->4 does not
+		{[]string{"first\r", "\nsecond\r", "\nthird\r", "fourth"}, "first\nsecond\nthird\rfourth"},
+		{[]string{"first\r", "\nsecond\r", "\nthird\n", "fourth"}, "first\nsecond\nthird\nfourth"},
+
 		{[]string{"this is the result\r\nfrom the first read\r", "\nthis is the result\r\nfrom the second read\r"},
 			"this is the result\nfrom the first read\nthis is the result\nfrom the second read\r"},
 		{[]string{"now is the time\r\nfor all good engineers\r\nto improve their test coverage!\r\n"},
