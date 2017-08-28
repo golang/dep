7f19f8ba9b92c45459515d5d91a2034764c83849
Spencer Nelson <spencer@twitch.tv>
2017-08-28 13:54:06 -0400

    Use structs for subtests instead of a function literal

--- internal/gps/source_manager_test.go
+++ internal/gps/source_manager_test.go
@@ -14,8 +14,75 @@ import (
 func TestSourceManager_InferConstraint(t *testing.T) {
 	t.Parallel()
 
-	testcase := func(str string, pi ProjectIdentifier, want Constraint) func(*testing.T) {
-		return func(t *testing.T) {
+	// Used in git subtests:
+	v081, err := NewSemverConstraintIC("v0.8.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	v012, err := NewSemverConstraintIC("v0.12.0-12-de4dcafe0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Used in hg and bzr subtests:
+	v1, err := NewSemverConstraintIC("v1.0.0")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var (
+		gitProj = ProjectIdentifier{ProjectRoot: "github.com/carolynvs/deptest"}
+		bzrProj = ProjectIdentifier{ProjectRoot: "launchpad.net/govcstestbzrrepo"}
+		hgProj  = ProjectIdentifier{ProjectRoot: "bitbucket.org/golang-dep/dep-test"}
+		svnProj = ProjectIdentifier{ProjectRoot: "bitbucket.org/golang-dep/dep-test"}
+
+		testcases = []struct {
+			project ProjectIdentifier
+			name    string
+			str     string
+			want    Constraint
+		}{
+			{gitProj, "empty", "", Any()},
+			{gitProj, "semver-short", "v0.8.1", v081},
+			{gitProj, "long semver constraint", "v0.12.0-12-de4dcafe0", v012},
+			{gitProj, "branch v2", "v2", NewBranch("v2")},
+			{gitProj, "branch master", "master", NewBranch("master")},
+			{gitProj, "long revision", "3f4c3bea144e112a69bbe5d8d01c1b09a544253f",
+				Revision("3f4c3bea144e112a69bbe5d8d01c1b09a544253f")},
+			{gitProj, "short revision", "3f4c3bea",
+				Revision("3f4c3bea144e112a69bbe5d8d01c1b09a544253f")},
+
+			{bzrProj, "empty", "", Any()},
+			{bzrProj, "semver", "v1.0.0", v1},
+			{bzrProj, "revision", "matt@mattfarina.com-20150731135137-pbphasfppmygpl68",
+				Revision("matt@mattfarina.com-20150731135137-pbphasfppmygpl68")},
+
+			{hgProj, "empty", "", Any()},
+			{hgProj, "semver", "v1.0.0", v1},
+			{hgProj, "default branch", "default", NewBranch("default")},
+			{hgProj, "revision", "6f55e1f03d91f8a7cce35d1968eb60a2352e4d59",
+				Revision("6f55e1f03d91f8a7cce35d1968eb60a2352e4d59")},
+			{hgProj, "short revision", "6f55e1f03d91",
+				Revision("6f55e1f03d91f8a7cce35d1968eb60a2352e4d59")},
+		}
+	)
+
+	for _, tc := range testcases {
+		var subtestName string
+		switch tc.project {
+		case gitProj:
+			subtestName = "git-" + tc.name
+		case bzrProj:
+			subtestName = "bzr-" + tc.name
+		case hgProj:
+			subtestName = "hg-" + tc.name
+		case svnProj:
+			subtestName = "svn-" + tc.name
+		default:
+			subtestName = tc.name
+		}
+
+		t.Run(subtestName, func(t *testing.T) {
 			t.Parallel()
 			h := test.NewHelper(t)
 			defer h.Cleanup()
@@ -26,69 +93,17 @@ func TestSourceManager_InferConstraint(t *testing.T) {
 			sm, err := NewSourceManager(h.Path(cacheDir))
 			h.Must(err)
 
-			got, err := sm.InferConstraint(str, pi)
+			got, err := sm.InferConstraint(tc.str, tc.project)
 			h.Must(err)
 
-			wantT := reflect.TypeOf(want)
+			wantT := reflect.TypeOf(tc.want)
 			gotT := reflect.TypeOf(got)
 			if wantT != gotT {
-				t.Errorf("expected type: %s, got %s, for input %s", wantT, gotT, str)
+				t.Errorf("expected type: %s, got %s, for input %s", wantT, gotT, tc.str)
 			}
-			if got.String() != want.String() {
-				t.Errorf("expected value: %s, got %s for input %s", want, got, str)
+			if got.String() != tc.want.String() {
+				t.Errorf("expected value: %s, got %s for input %s", tc.want, got, tc.str)
 			}
-		}
+		})
 	}
-
-	var (
-		gitProj = ProjectIdentifier{ProjectRoot: "github.com/carolynvs/deptest"}
-		bzrProj = ProjectIdentifier{ProjectRoot: "launchpad.net/govcstestbzrrepo"}
-		hgProj  = ProjectIdentifier{ProjectRoot: "bitbucket.org/golang-dep/dep-test"}
-	)
-
-	t.Run("git", func(t *testing.T) {
-		t.Parallel()
-		t.Run("empty", testcase("", gitProj, Any()))
-
-		v081, err := NewSemverConstraintIC("v0.8.1")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		v012, err := NewSemverConstraintIC("v0.12.0-12-de4dcafe0")
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		t.Run("semver constraint", testcase("v0.8.1", gitProj, v081))
-		t.Run("long semver constraint", testcase("v0.12.0-12-de4dcafe0", gitProj, v012))
-		t.Run("branch v2", testcase("v2", gitProj, NewBranch("v2")))
-		t.Run("branch master", testcase("master", gitProj, NewBranch("master")))
-		t.Run("long revision", testcase("3f4c3bea144e112a69bbe5d8d01c1b09a544253f", gitProj, Revision("3f4c3bea144e112a69bbe5d8d01c1b09a544253f")))
-		t.Run("short revision", testcase("3f4c3bea", gitProj, Revision("3f4c3bea144e112a69bbe5d8d01c1b09a544253f")))
-	})
-
-	t.Run("bzr", func(t *testing.T) {
-		t.Parallel()
-		v1, err := NewSemverConstraintIC("v1.0.0")
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Run("empty", testcase("", bzrProj, Any()))
-		t.Run("semver", testcase("v1.0.0", bzrProj, v1))
-		t.Run("revision", testcase("matt@mattfarina.com-20150731135137-pbphasfppmygpl68", bzrProj, Revision("matt@mattfarina.com-20150731135137-pbphasfppmygpl68")))
-	})
-
-	t.Run("hg", func(t *testing.T) {
-		t.Parallel()
-		v1, err := NewSemverConstraintIC("v1.0.0")
-		if err != nil {
-			t.Fatal(err)
-		}
-		t.Run("empty", testcase("", hgProj, Any()))
-		t.Run("semver", testcase("v1.0.0", hgProj, v1))
-		t.Run("default branch", testcase("default", hgProj, NewBranch("default")))
-		t.Run("revision", testcase("6f55e1f03d91f8a7cce35d1968eb60a2352e4d59", hgProj, Revision("6f55e1f03d91f8a7cce35d1968eb60a2352e4d59")))
-		t.Run("short revision", testcase("6f55e1f03d91", hgProj, Revision("6f55e1f03d91f8a7cce35d1968eb60a2352e4d59")))
-	})
 }
