412b69be65247c56ad60175f618614334bb2f631
Tamir Duberstein <tamird@gmail.com>
2017-09-18 19:44:44 -0400

    gps: DRY

--- internal/gps/cmd_unix.go
+++ internal/gps/cmd_unix.go
@@ -21,24 +21,16 @@ type cmd struct {
 	ctx context.Context
 	// cancel is called when the graceful shutdown timeout expires.
 	cancel context.CancelFunc
-	cmd    *exec.Cmd
+	Cmd    *exec.Cmd
 }
 
 func commandContext(ctx context.Context, name string, arg ...string) cmd {
 	// Grab the caller's context and pass a derived one to CommandContext.
 	c := cmd{ctx: ctx}
 	ctx, cancel := context.WithCancel(ctx)
-	c.cmd = exec.CommandContext(ctx, name, arg...)
+	c.Cmd = exec.CommandContext(ctx, name, arg...)
 	c.cancel = cancel
 	return c
-}
-
-func (c cmd) Args() []string {
-	return c.cmd.Args
-}
-
-func (c cmd) SetDir(dir string) {
-	c.cmd.Dir = dir
 }
 
 // CombinedOutput is like (*os/exec.Cmd).CombinedOutput except that it
@@ -46,16 +38,16 @@ func (c cmd) SetDir(dir string) {
 // the subprocess fails to exit after 1 minute.
 func (c cmd) CombinedOutput() ([]byte, error) {
 	// Adapted from (*os/exec.Cmd).CombinedOutput
-	if c.cmd.Stdout != nil {
+	if c.Cmd.Stdout != nil {
 		return nil, errors.New("exec: Stdout already set")
 	}
-	if c.cmd.Stderr != nil {
+	if c.Cmd.Stderr != nil {
 		return nil, errors.New("exec: Stderr already set")
 	}
 	var b bytes.Buffer
-	c.cmd.Stdout = &b
-	c.cmd.Stderr = &b
-	if err := c.cmd.Start(); err != nil {
+	c.Cmd.Stdout = &b
+	c.Cmd.Stderr = &b
+	if err := c.Cmd.Start(); err != nil {
 		return nil, err
 	}
 
@@ -71,7 +63,7 @@ func (c cmd) CombinedOutput() ([]byte, error) {
 	go func() {
 		select {
 		case <-c.ctx.Done():
-			if err := c.cmd.Process.Signal(os.Interrupt); err != nil {
+			if err := c.Cmd.Process.Signal(os.Interrupt); err != nil {
 				// If an error comes back from attempting to signal, proceed
 				// immediately to hard kill.
 				c.cancel()
@@ -82,7 +74,7 @@ func (c cmd) CombinedOutput() ([]byte, error) {
 		}
 	}()
 
-	if err := c.cmd.Wait(); err != nil {
+	if err := c.Cmd.Wait(); err != nil {
 		return nil, err
 	}
 	return b.Bytes(), nil
