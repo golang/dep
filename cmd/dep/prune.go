142735b5947d4948766edce0a3522639c23b4b67
sam boyer <tech@samboyer.org>
2018-01-10 16:12:43 -0800

    prune: Tweak help and output wording a bit

--- cmd/dep/prune.go
+++ cmd/dep/prune.go
@@ -10,10 +10,11 @@ import (
 	"github.com/golang/dep"
 )
 
-const pruneShortHelp = `Prune was merged into ensure. Use ensure instead.`
+const pruneShortHelp = `Pruning is now performed automatically by dep ensure.`
 const pruneLongHelp = `
 Prune was merged into the ensure command.
 Set prune options in the manifest and it will be applied after every ensure.
+dep prune will be removed in a future version of dep, causing this command to exit non-0.
 `
 
 type pruneCommand struct{}
@@ -27,8 +28,9 @@ func (cmd *pruneCommand) Hidden() bool      { return true }
 func (cmd *pruneCommand) Register(fs *flag.FlagSet) {}
 
 func (cmd *pruneCommand) Run(ctx *dep.Ctx, args []string) error {
-	ctx.Out.Printf("Prune was merged into ensure.\n")
+	ctx.Out.Printf("Pruning is now performed automatically by dep ensure.\n")
 	ctx.Out.Printf("Set prune settings in %s and it it will be applied when running ensure.\n", dep.ManifestName)
+	ctx.Out.Printf("\ndep prune will be removed in a future version, and this command will exit non-0.\nPlease update your scripts.\n")
 
 	return nil
 }
