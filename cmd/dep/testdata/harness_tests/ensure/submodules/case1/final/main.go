d338aaffd69d8cb63dc5258daa6ced02aa76777f
Patrick Ohly <patrick.ohly@intel.com>
2019-01-03 15:56:27 +0100

    dep: add submodule testcase
    This new test fails if the content of two recursive submodules is not
    exported. Verification depends on comparing the entire "final"
    directory.

---
+++ cmd/dep/testdata/harness_tests/ensure/submodules/case1/final/main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	_ "github.com/pohly/deptestmodules/pkg/foo"
+)
+
+func main() {
+}
---
+++ cmd/dep/testdata/harness_tests/ensure/submodules/case1/final/vendor/github.com/pohly/deptestmodules/pkg/foo/foo.go
@@ -0,0 +1,7 @@
+/*
+Copyright 2018 Intel Corporation.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package foo
