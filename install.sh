5ae9d8b3c2b6e68f5732a8dd62bab173e26f0f03
Karl Skewes <karl.skewes@gmail.com>
2019-02-01 08:03:44 +1300

    feat(arm): Add build and install support for armv6 and arm64
    On path to close #1807 on [oauth2_proxy arm support](https://github.com/pusher/oauth2_proxy/issues/16)
    
    Tested:
    - Building armv6 and arm64 binaries on amd64
    - Running arm binaries
    - Using armv6 binary on RPi 3B to build another project [oauth2_proxy](https://github.com/kskewes/oauth2_proxy/tree/build)
      (`dep ensure`)
    
    Not verified:
    - Install script completes as binaries not published, looks okay
    though.
    - `hack/test.bash` - must have missed something.
    
    armv6 (RPi 3B):
    ```
    karl@pi1:~/go/src/github.com/pusher/oauth2_proxy $ dep version
    dep:
     version     : v0.5.0-31-g73b3afef-dirty
     build date  : 2019-02-01
     git hash    : 73b3afef
     go version  : go1.11.5
     go compiler : gc
     platform    : linux/arm
     features    : ImportDuringSolve=false
    
    karl@pi1:~/go/src/github.com/pusher/oauth2_proxy $ make dep
    dep ensure --vendor-only
    
    karl@pi1:~/go/src/github.com/pusher/oauth2_proxy $ uname -m
    armv7l
    
    karl@pi1:~ $ ./install.sh
    ARCH = arm
    OS = linux
    Will install into /home/karl/go/bin
    Fetching https://github.com/golang/dep/releases/latest..
    Release Tag = v0.5.0
    Fetching https://github.com/golang/dep/releases/tag/v0.5.0..
    Fetching https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-arm..
    Request failed with code 404
    
    karl@pi1:~ $ ./test.bash
    go test: -race is only supported on linux/amd64, linux/ppc64le, freebsd/amd64, netbsd/amd64, darwin/amd64 and windows/amd64
    ```
    
    arm64 (Rock64):
    ```
    rock64@k8s-w-01:~$ ./dep-linux-arm64 version
    dep:
     version     : v0.5.0-31-g73b3afef-dirty
     build date  : 2019-02-01
     git hash    : 73b3afef
     go version  : go1.11.5
     go compiler : gc
     platform    : linux/arm64
     features    : ImportDuringSolve=false
    
    rock64@k8s-w-01:~$ uname -m
    aarch64
    
    rock64@k8s-w-01:~$ ./install.sh
    ARCH = arm64
    OS = linux
    Will install into /home/rock64/go/bin
    Fetching https://github.com/golang/dep/releases/latest..
    Release Tag = v0.5.0
    Fetching https://github.com/golang/dep/releases/tag/v0.5.0..
    Fetching https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-arm64..
    Request failed with code 404
    ```

--- install.sh
+++ install.sh
@@ -97,6 +97,9 @@ initArch() {
         ppc64) ARCH="ppc64";;
         ppc64le) ARCH="ppc64le";;
         s390x) ARCH="s390x";;
+        armv6*) ARCH="arm";;
+        armv7*) ARCH="arm";;
+        aarch64) ARCH="arm64";;
         *) echo "Architecture ${ARCH} is not supported by this installation script"; exit 1;;
     esac
     echo "ARCH = $ARCH"
