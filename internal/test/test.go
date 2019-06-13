23b2e86204ca271f23cd792ee6ddc918b6ac03ad
Kevin Burke <kevin@burke.dev>
2019-06-13 09:16:50 -0700

    hack,internal: fix staticcheck
    The most recent version of staticcheck drops the "--ignore" flag in
    favor of inline ignore directives, so we need to use those or fix on
    the older version.
    
    Good news is two of our ignore directives are now no longer needed.
    
    Updates dominikh/go-tools#514.
    Updates dominikh/go-tools#515.
    Fixes #2178.

--- internal/test/test.go
+++ internal/test/test.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//lint:file-ignore U1000 unused fns we might want to use later.
+
 package test
 
 import (
